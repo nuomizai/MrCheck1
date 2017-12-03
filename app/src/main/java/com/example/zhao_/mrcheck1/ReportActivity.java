ad158400950535f691f6829d4b4efcb1d2b3c45f
King J <1185657109@qq.com>
2017-12-03 22:37:39 +0800

    report_form1

---
+++ app/src/main/java/com/example/zhao_/mrcheck1/ReportActivity.java
@@ -0,0 +1,16 @@
+package com.example.zhao_.mrcheck1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by J on 2017/12/3.
+ */
+
+public class ReportActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report_form);
+    }
+}
