80cf115e55cd43fe1b436d18179d723b503cb084
Graceqi <1792197177@qq.com>
2017-12-05 20:34:18 +0800

    12

--- app/src/main/java/com/example/zhao_/mrcheck1/MainActivity.java
+++ app/src/main/java/com/example/zhao_/mrcheck1/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.zhao_.mrcheck1;
 /**
  * Created by 万珂嘉 on 2017/12/3.
  */
+import android.content.Intent;
 import android.os.Message;
 import java.util.Calendar;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.app.Activity;
 import android.app.DatePickerDialog;
@@ -24,7 +32,8 @@ import org.json.*;
 import okhttp3.*;
 import java.io.IOException;
 
-public class MainActivity  extends  AppCompatActivity {
+public class MainActivity  extends  AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
     private Button add,btn;
     private EditText money,ps;
     private String money1,ps1,spstr;
@@ -49,10 +58,23 @@ public class MainActivity  extends  AppCompatActivity {
             }
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 /**
  * 初始化数据
  */
@@ -110,6 +132,55 @@ public class MainActivity  extends  AppCompatActivity {
 
         //显示当前时间
         updateDisplay();
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_checkbook) {
+
+            // Handle the camera action
+        } else if (id == R.id.nav_search) {
+
+        } else if (id == R.id.nav_report) {
+
+        } else if (id == R.id.nav_message) {
+            Intent intent = new Intent();
+            intent.setClass(this, MyinforActivity.class);
+            this.startActivity(intent);
+
+        } else if (id == R.id.nav_quit) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
     @Override
     protected Dialog onCreateDialog(int id) {
---
+++ app/src/main/java/com/example/zhao_/mrcheck1/MyinforActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhao_.mrcheck1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyinforActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myinfor);
+    }
+}
